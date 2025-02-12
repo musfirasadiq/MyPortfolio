50e1bc0301733ad75f00910e51c9c9877225be7d
Musfira L A <musfira443@gmail.com>
2025-02-12 23:01:20 +0400

    Updated hero section for responsiveness

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [MatGridListModule,
+    MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
